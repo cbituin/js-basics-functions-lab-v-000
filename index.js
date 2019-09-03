f3c936b8e195226afa789fa0efbcacd74879f5d0
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2019-09-03 14:24:44 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,16 @@
+// Code your solution in this file!
+function distanceFromHqInBlocks() {
+
+}
+
+function distanceTravelledInFeet() {
+
+}
+
+function distanceTravelledInFeet() {
+
+}
+
+function calculatesFarePrice() {
+
+}
