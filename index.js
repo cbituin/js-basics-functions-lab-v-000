cc13b50967fccd39f312a666f302c559feb284dd
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2019-09-03 14:41:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 // Code your solution in this file!
 function distanceFromHqInBlocks(currentBlock) {
-  let distance = Math.abs(42 - currentBlock)
-  return distance
+  let distance = Math.abs(42 - currentBlock);
+  return distance;
 }
 
 function distanceFromHqInFeet(currentBlock) {
- let distance = distanceFromHqInBlocks(currentBlock) * 264
- return distance
+ let distance = distanceFromHqInBlocks(currentBlock) * 264;
+ return distance;
 }
 
-function distanceTravelledInFeet() {
-
+function distanceTravelledInFeet(start, current) {
+  let numBlocks = Math.abs(start - current);
+  let distance = numBlocks * 264;
+  return distance  
 }
 
 function calculatesFarePrice() {
